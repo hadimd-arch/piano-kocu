5255a1889048c15cf0ca362fe0ba1f7c234cce3b
hadimd-arch <hadiozyasar@gmail.com>
2026-07-19 17:00:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Piyano Koçu SW — ağ öncelikli gezinme: güncellemeler otomatik, çevrimdışı yedekli
-const V = "piyano-kocu-v40";
+const V = "piyano-kocu-v41";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
